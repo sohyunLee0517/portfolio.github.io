31c11ccf54d9f43e6ce3f79ceb01f1b0e833670f
이소현 <isohyeon@isohyeons-MacBook-Pro.local>
2022-06-20 23:00:28 +0900

    interection

--- src/components/career.vue
+++ src/components/career.vue
@@ -4,7 +4,7 @@
         CAREER
     </div>
     <div class="section-wrap">
-      <div class="section-box" v-for="list in careerList">
+      <div class="section-box motion" v-for="list in careerList" @click="moveTo(list.url)">
         <div class="section-box-title">{{list.title}}</div>
         <div class="section-box-sub-title">{{list.period}}</div>
         <img class="section-box-img" :src="list.img">
@@ -24,15 +24,22 @@ export default {
           title : 'pnp.so',
           period : '2021.03 ~ 2021.03',
           img : require('../assets/img/pnpso.png'),
-          desc : 'pnp.so 에서 기존 html 페이지를 Vue.js로 변환하는 작업을 하였습니다.<br>칸반, 캘린더, 타임라인, 채팅, 웹문서 등 다양한 방식의 화면 개발을 경험했습니다.<br>시스템 특성상 모바일기기에서도 동일하게 보일 수 있도록, 반응형으로 제작하였습니다.'
+          desc : 'pnp.so 에서 기존 html 페이지를 Vue.js로 변환하는 작업을 하였습니다.<br>칸반, 캘린더, 타임라인, 채팅, 웹문서 등 다양한 방식의 화면 개발을 경험했습니다.<br>시스템 특성상 모바일기기에서도 동일하게 보일 수 있도록, 반응형으로 제작하였습니다.',
+          url : 'https://pnp.so'
         },
         {
           title : 'habil.so',
           period : '2021.04 ~ 2021.6',
           img : require('../assets/img/habilso.png'),
-          desc : 'pnp.so의 단순버전으로, 기존 사용하던 Vue.js를 보다 더 효과적으로 사용할 수 있었습니다.<br>Vue.js로 프로젝트 셋팅(컴포넌트 구조, 파일 구조, 라우터)을 하였습니다.<br>Vuex를 본격적으로 front에 도입했으며, 전체 페이지 frontEnd를 담당하였습니다.'
+          desc : 'pnp.so의 단순버전으로, 기존 사용하던 Vue.js를 보다 더 효과적으로 사용할 수 있었습니다.<br>Vue.js로 프로젝트 셋팅(컴포넌트 구조, 파일 구조, 라우터)을 하였습니다.<br>Vuex를 본격적으로 front에 도입했으며, 전체 페이지 frontEnd를 담당하였습니다.',
+          url : 'https://habil.so'
         }
       ]
+    }
+  },
+  methods : {
+    moveTo : (url) => {
+      window.open(url)
     }
   }
 }
--- src/components/header.vue
+++ src/components/header.vue
@@ -4,31 +4,68 @@
         <img src="../assets/img/logo_dark.svg" class="logoImg">
     </div>
     <div class="menu">
-        <div @click="moveToScroll('aboutMe')">About me</div>
-        <div @click="moveToScroll('skills')">Skills</div>
-        <div @click="moveToScroll('archiving')">Archiving</div>
-        <div @click="moveToScroll('projects')">Projects</div>
-        <div @click="moveToScroll('career')">Career</div>
+        <div @click="moveToScroll('aboutMe')" :class="{'active' : scrollSection == 'aboutme'}">About me</div>
+        <div @click="moveToScroll('skills')" :class="{'active' : scrollSection == 'skills'}">Skills</div>
+        <div @click="moveToScroll('archiving')" :class="{'active' : scrollSection == 'archiving'}">Archiving</div>
+        <div @click="moveToScroll('projects')" :class="{'active' : scrollSection == 'projects'}">Projects</div>
+        <div @click="moveToScroll('career')" :class="{'active' : scrollSection == 'career'}">Career</div>
     </div>
 </template>
 
 <script>
 export default {
   name: 'MainHeader',
-  props: {
+  data(){
+    return{
+        scrollSection : ''
+    }
+  },
+  mounted() {
+
+      document.getElementsByTagName('main')[0].addEventListener('scroll', () => {
+        let scrolltop = document.getElementsByTagName('main')[0].scrollTop;
+        let maintop = document.getElementsByClassName('main-masthead')[0].scrollHeight - 100;
+        let aboutmetop = maintop + document.getElementsByClassName('aboutMe')[0].scrollHeight - 100;
+        let skillstop = aboutmetop + document.getElementsByClassName('skills')[0].scrollHeight - 100;
+        let archivingtop = skillstop + document.getElementsByClassName('archiving')[0].scrollHeight - 100;
+        let projectstop = archivingtop + document.getElementsByClassName('projects')[0].scrollHeight - 100;
+        let careertop = projectstop + document.getElementsByClassName('career')[0].scrollHeight - 100;
+        
+        if(scrolltop < maintop){
+            this.scrollSection = 'main'
+        }else if(scrolltop > maintop && scrolltop < aboutmetop){
+            this.scrollSection = 'aboutme'
+        }else if(scrolltop > aboutmetop && scrolltop < skillstop){
+            this.scrollSection = 'skills'
+        } else if(scrolltop > skillstop && scrolltop < archivingtop){
+            this.scrollSection = 'archiving'
+        }else if(scrolltop > archivingtop && scrolltop < projectstop){
+            this.scrollSection = 'projects'
+        }else if(scrolltop > projectstop){
+            this.scrollSection = 'career'
+        }
+
+      })
   },
   methods : {
-      moveToScroll : (id, event) => {
+
+      moveToScroll : (className, event) => {
         let el = document.getElementsByTagName('main')[0];
 
-        if(id == 'top'){
+        if(className == 'top'){
             return el.scrollTop = 0
         }
-        let nextEl = document.getElementsByClassName(id)[0].getBoundingClientRect().top;
+        let nextEl = document.getElementsByClassName(className)[0].getBoundingClientRect().top;
 
         el.scrollTop = el.scrollTop + nextEl;
 
       }
+  },
+  watch : {
+    scrollSection : (newVal, oldVal) => {
+        console.log(newVal);
+    }
   }
+
 }
 </script>
